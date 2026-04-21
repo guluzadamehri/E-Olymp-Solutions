1b54b2ca7b974c50bcb791200be840ca677014fe
guluzadamehri <guluzadamehri@gmail.com>
2026-04-21 09:13:34 +0400

    Create 19.py

---
+++ Leetcode /19.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        if not head:
+        	return None
+
+        ref = head
+        while n > 0:
+        	ref = ref.next
+        	n -= 1
+
+        if ref is None:
+        	return head.next
+        else:
+        	main = head
+        	while ref.next:
+        		main = main.next
+        		ref = ref.next
+
+        	main.next = main.next.next
+        	return head
